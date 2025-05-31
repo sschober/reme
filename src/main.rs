6bb45dbf362af8402310925a0f0e33f82676360c
Sven Schober <sv3sch@gmail.com>
2025-05-31 20:35:31 +0200

    introduce rc to implement append

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
-use std::fmt::{self};
+//! reme is my interpretation of a `rust scheme`
+use std::{fmt, rc::Rc};
 
+#[derive(Debug, Clone, PartialEq)]
 pub enum List {
     Empty(),
     Lit(String),
-    Pair(Box<List>, Box<List>),
+    Pair(Rc<List>, Rc<List>),
 }
 impl List {
     fn internal_fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            List::Empty() => {
-                write!(f, "")
-            }
+            List::Empty() => write!(f, ""),
             List::Lit(s) => write!(f, "'{}'", s),
             List::Pair(h, t) => {
-                _ = (*h).internal_fmt(f);
+                (*h).internal_fmt(f)?;
                 match **t {
                     List::Empty() => write!(f, "")?,
                     _ => write!(f, " ")?,
@@ -25,8 +25,8 @@ impl List {
 }
 impl fmt::Display for List {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        _ = write!(f, "(");
-        _ = self.internal_fmt(f);
+        write!(f, "(")?;
+        self.internal_fmt(f)?;
         write!(f, ")")
     }
 }
@@ -47,28 +47,66 @@ macro_rules! list {
         cons(lit($head), list!($($tail),*))
     };
 }
+#[macro_export]
+macro_rules! list_rc {
+    () => { list() };
+    ($value:expr) => {
+        // TODO we might drop the lit as the caller should decide, what we want to add
+        cons_rc(Rc::new(lit($value)), Rc::new(list()))
+    };
+    ($head:expr, $($tail:expr),*) => {
+        // TODO we might drop the lit as the caller should decide, what we want to add
+        cons_rc(Rc::new(lit($head)), list_rc!($($tail),*))
+    };
+}
+/// cons prepends a new element to a list
 pub fn cons(a: List, b: List) -> List {
-    List::Pair(Box::new(a), Box::new(b))
+    List::Pair(Rc::new(a), Rc::new(b))
+}
+pub fn cons_rc(a: Rc<List>, b: Rc<List>) -> Rc<List> {
+    Rc::new(List::Pair(a, b))
+}
+/// append appends list b to list a
+pub fn append(a: Rc<List>, b: Rc<List>) -> Rc<List> {
+    match *a {
+        List::Empty() => b,
+        _ => cons_rc(car_rc(Rc::clone(&a)), append(cdr_rc(a), b)),
+    }
 }
 pub fn is_pair(l: List) -> bool {
     matches!(l, List::Pair(_, _))
 }
-pub fn car(l: List) -> List {
-    match l {
-        List::Pair(h, _) => *h,
-        _ => List::Empty(),
+pub fn car_rc(l: Rc<List>) -> Rc<List> {
+    // I find this construction, taking a ref of a dereferenced Rc especially ugly.
+    // I only found this after googling around. If we do not use this contraption,
+    // the compiler complains, that h is moved out of l.
+    match &*l {
+        List::Pair(h, _) => Rc::clone(h),
+        _ => Rc::new(List::Empty()),
     }
 }
-pub fn cdr(l: List) -> List {
+pub fn cdr_rc(l: Rc<List>) -> Rc<List> {
+    match &*l {
+        List::Pair(_, t) => Rc::clone(t),
+        _ => Rc::new(List::Empty()),
+    }
+}
+pub fn car(l: &List) -> &List {
     match l {
-        List::Pair(_, t) => *t,
-        _ => List::Empty(),
+        List::Pair(h, _) => h,
+        _ => &List::Empty(),
+    }
+}
+pub fn cdr(l: &List) -> &List {
+    match l {
+        List::Pair(_, t) => t,
+        _ => &List::Empty(),
     }
 }
 pub fn list() -> List {
     List::Empty()
 }
-pub fn length(l: List) -> usize {
+pub fn length(l: &List) -> usize {
     match l {
         List::Empty() => 0,
         _ => 1 + length(cdr(l)),
@@ -79,14 +117,16 @@ fn main() {
 }
 #[cfg(test)]
 mod tests {
-    use crate::{length, lit};
+    use std::rc::Rc;
 
-    use super::{cons, list};
+    use crate::{append, car_rc, length, lit};
+
+    use super::{cons, cons_rc, list};
 
     #[test]
     fn empty_list_is_size_0() {
         let l = list();
-        assert_eq!(0, length(l));
+        assert_eq!(0, length(&l));
     }
     #[test]
     fn empty_list_displays_correctly() {
@@ -96,7 +136,7 @@ mod tests {
     #[test]
     fn cons_1_to_empty_list() {
         let l = cons(lit("1"), list());
-        assert_eq!(1, length(l))
+        assert_eq!(1, length(&l))
     }
     #[test]
     fn cons_1_displays_correctly() {
@@ -106,7 +146,7 @@ mod tests {
     #[test]
     fn cons_2_to_list_of_1_has_size_2() {
         let l = cons(lit("2"), cons(lit("1"), list()));
-        assert_eq!(2, length(l))
+        assert_eq!(2, length(&l))
     }
     #[test]
     fn cons_2_displays_correctly() {
@@ -129,6 +169,40 @@ mod tests {
     #[test]
     fn list_marcro_works_for_2() {
         let l = list!("1", "2");
+        eprintln!("{}", l);
+        eprintln!("{:?}", l);
         assert_eq!("('1' '2')", format!("{}", l));
+    }
+
+    #[test]
+    fn append_1_to_empty() {
+        let l = list();
+        let n = list!("1");
+        let a = append(Rc::new(l), Rc::new(n));
+        eprintln!("a: {}", a);
+        eprintln!("a: {:?}", a);
+        assert_eq!("('1')", format!("{}", a))
+    }
+
+    #[test]
+    fn append_2_to_1() {
+        let l = list_rc!("1");
+        let n = list_rc!("2");
+        let a = append(Rc::clone(&l), n);
+
+        eprintln!("a: {}", a);
+        eprintln!("a: {:?}", a);
+        assert_eq!("('1' '2')", format!("{}", a));
+        // the * is necessary as car_rc returns an Rc
+        // car_rc is necessary as a itself is an Rc
+        assert_eq!(lit("1"), *car_rc(a));
+    }
+
+    #[test]
+    fn real_worldy_list() {
+        let l = list!("this", "is", "a", "list");
+
+        eprintln!("l: {}", l);
+        eprintln!("l: {:?}", l);
     }
 }
